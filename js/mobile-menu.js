7f41d839a8aedc816cb78375074ba72a67ae15e2
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-06-01 19:51:47 +0300

    2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
-  const navListRef = document.querySelector(".class-navigation-list");
-  const headerRef = document.querySelector(".class-header");
-  const navigationRef = document.querySelector(".class-navigation");
+  const navListRef = document.querySelector(".r-g-navigation-list");
+  const headerRef = document.querySelector(".r-g-header");
+  const navigationRef = document.querySelector(".r-g-navigation");
 
   menuBtnRef.addEventListener("click", () => {
     menuBtnRef.classList.toggle("is-open");
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     navigationRef.classList.toggle("is-open");
   });
 
-  document.querySelectorAll(".class-navigation-link").forEach((n) =>
+  document.querySelectorAll(".r-g-navigation-link").forEach((n) =>
     n.addEventListener("click", () => {
       navListRef.classList.remove("is-open");
       menuBtnRef.classList.remove("is-open");
@@ -23,15 +23,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function toggleAnswer(element) {
   const answer = element.nextElementSibling;
-  const buttonImg = element.querySelector(".class-toggle-button img");
+  const buttonImg = element.querySelector(".r-g-toggle-button img");
 
   answer.classList.toggle("open");
 
   if (answer.classList.contains("open")) {
-    buttonImg.src = "/img/Minus.png";
+    buttonImg.src = "/img/close.png";
     buttonImg.classList.add("rotated");
   } else {
-    buttonImg.src = "/img/Add.png";
+    buttonImg.src = "/img/cross.png";
     buttonImg.classList.remove("rotated");
   }
 }
